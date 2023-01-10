e9f3aed40c93e27778f3b0c7641a92bf9cdb37ac
Terli1337 <adrian.terlecki1@gmail.com>
2023-01-10 10:34:42 +0100

    js added

---
+++ js/script.js
@@ -0,0 +1,32 @@
+{
+    const tasks = [
+        {
+            content: "zjeść śniadanie",
+            done: true,
+        },
+        {
+            content: "zrobić pracę domową",
+            done: false,
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+            ${task.content}
+            </li>
+            `;
+        }
+        
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+    }
+
+    init();
+}
